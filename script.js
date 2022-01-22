ece474def2b378f95c34ed475058b7fef82404b8
Dom <97327625+9und30@users.noreply.github.com>
2022-01-22 15:17:06 +0100

    add more sections, navbar and form

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let learn_more_btn = document.getElementById('lm-btn')
-let second_sec = document.getElementById('second-sec')
+// nav elements
+let nav_about = document.getElementById('nav-about')
+let nav_projects = document.getElementById('nav-projects')
+let nav_contact = document.getElementById('nav-contact')
 
-learn_more_btn.addEventListener('click', () => {
-    second_sec.scrollIntoView({behavior: 'smooth'})
-})
+// buttons
+let learn_more_btn = document.getElementById('lm-btn')
+
+// sections
+let header_sec = document.getElementById('header-sec')
+let about_sec = document.getElementById('about-sec')
+let projects_sec = document.getElementById('projects-sec')
+let contact_sec = document.getElementById('contact-sec')
+
+
+function scrollToHeader() {
+    header_sec.scrollIntoView({behavior: 'smooth'})
+}
+
+function scrollToAbout() {
+    about_sec.scrollIntoView({behavior: 'smooth'})
+}
+
+function scrollToProjects() {
+    projects_sec.scrollIntoView({behavior: 'smooth'})
+}
+
+function scrollToContact() {
+    contact_sec.scrollIntoView({behavior: 'smooth'})
+}
